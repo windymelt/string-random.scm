f2c2f66a59c488dc0de1bd2a342b3d2815713381
Windymelt <qwilas@3qe.us>
2017-02-04 00:25:42 +0900

    impl

--- string-random2.scm
+++ string-random2.scm
@@ -1,5 +1,7 @@
 (use parser.peg)
 (use util.match)
+(use data.random)
+(use gauche.generator)
 
 (define %char ($none-of #[\]\)\|]))
 (define %atom ($or ($between ($c #\() ($lazy %regex) ($c #\))) %char))
@@ -11,18 +13,20 @@
 (define %branch ($many %piece 1))
 (define %regex ($lift (^[xs] (cons 'or xs)) ($sep-by %branch ($c #\|))))
 
-(define tgt "(nyaa|meow)")
+(define tgt "(ルイズ！)+((ルイズ！?)+|(ぅ*う*わぁ+あ+ん！+)|あ+ぁ+|ぅ+)+")
 (print (peg-parse-string %regex tgt))
 
 (define s "")
 (define (walk tree)
-  (match #?=tree
+  (match tree
     [() ""]
-    [(? char? c) ($ list->string $ list #?=c)]
+    [(? char? c) ($ list->string $ list c)]
     [(#f . c) (walk c)]
-    [('? . xs) ""]
-    [('+ . xs) (walk xs)]
-    [('or . xs) (walk (car xs))]
+    [('? . xs) (if booleans (walk xs) "")]
+    [('+ . xs) (walk (make-list (car (generator->list (integers$ 6 1) 1)) xs))]
+    [('* . xs) (walk (make-list (car (generator->list (integers$ 6 0) 1)) xs))]
+    [('or . xs) (walk (car (generator->list (samples$ xs) 1)))]
     [(x . xs) (string-append (walk x) (walk xs))]))
 
+;(set! (random-data-seed) 42)
 (print (walk (peg-parse-string %regex tgt)))
